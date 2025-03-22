d03cf424cc66d98435720381b80ac4b960b90c0c
unknown <ernestoiii.balignasay@tup.edu.ph>
2025-03-22 20:03:55 +0800

    login fixed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,15 +2,45 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    firebaseUid: { type: String, required: true, unique: true },
-    password: { type: String, required: true, default: "firebase-manage" }, // Handled by Firebase
-    phoneNumber: { type: String, required: true },
-    address: { type: String },
-    avatar: { type: String, default: '' }, // Cloudinary URL
-    isAdmin: { type: Boolean, default: false }, // 0: User, 1: Admin
-    fcmToken: { type: String, default: '' }, // Push Notification Token
+    name:  { 
+      type: String, 
+      required: true 
+    
+    },
+    email:  { 
+      type: String, 
+      required: true, 
+      unique: true 
+    },
+    firebaseUid: { 
+      type: String, 
+      required: true, 
+      unique: true 
+    },
+    password: { 
+      type: String, 
+      required: true, 
+      default: "firebase-manage" 
+    }, 
+    phoneNumber:  { 
+      type: String, 
+      required: true 
+    },
+    address: { 
+      type: String 
+    },
+    avatar: {
+       type: String, 
+       default: ''
+       }, 
+    isAdmin: {
+       type: Boolean, 
+       default: false 
+      },
+    fcmToken: {
+       type: String,
+        default: '' 
+      },
   },
   { timestamps: true }
 );
